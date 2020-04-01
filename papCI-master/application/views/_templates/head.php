29dac8b854e589820868ec56ada100e6abf6653b
Victor Rivera <victor.rivera4@gmail.com>
2020-04-01 04:33:38 +0200

    Segundo Commit con cambios no importantes

--- papCI-master/application/views/_templates/head.php
+++ papCI-master/application/views/_templates/head.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
 	
 	<script src="<?=base_url()?>assets/js/jquery.min.js" ></script>
-	<!-- <script src="<?=base_url()?>assets/js/popper.min.js" ></script>  -->
+	<script src="<?=base_url()?>assets/js/popper.min.js" ></script>
 	<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 	
 	<title>Mi título</title>
